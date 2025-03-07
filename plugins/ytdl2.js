19968e0610f9abe3757879f188c4cb084acdb074
KermRayan <kermd237@gmail.com>
2025-03-07 16:25:22 +0100

    Update ytdl2.js

--- plugins/ytdl2.js
+++ plugins/ytdl2.js
@@ -3,74 +3,7 @@ const {
   commands
 } = require("../command");
 const yts = require("yt-search");
-const {
-  fetchJson
-} = require("../lib/functions");
 const axios = require('axios');
-
-async function ytmp4(url, format) {
-  try {
-    if (!url || !format) {
-      throw new Error("url and format parameters are required.");
-    }
-    const parsedFormat = parseInt(format.replace('p', ''), 10);
-    const params = {
-      'button': 1,
-      'start': 1,
-      'end': 1,
-      'format': parsedFormat,
-      'url': url
-    };
-    const headers = {
-      'Accept': '*/*',
-      'Accept-Encoding': "gzip, deflate, br",
-      'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
-      'Origin': 'https://loader.to',
-      'Referer': "https://loader.to",
-      'Sec-Ch-Ua': "\"Not-A.Brand\";v=\"99\", \"Chromium\";v=\"124\"",
-      'Sec-Ch-Ua-Mobile': '?1',
-      'Sec-Ch-Ua-Platform': "\"Android\"",
-      'Sec-Fetch-Dest': "empty",
-      'Sec-Fetch-Mode': "cors",
-      'Sec-Fetch-Site': "cross-site",
-      'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
-    };
-    const response = await axios.get('https://ab.cococococ.com/ajax/download.php', {
-      params: params,
-      headers: headers
-    });
-    const downloadId = response.data.id;
-    const checkProgress = async () => {
-      const progressParams = {
-        'id': downloadId
-      };
-      try {
-        const progressResponse = await axios.get("https://p.oceansaver.in/ajax/progress.php", {
-          params: progressParams,
-          headers: headers
-        });
-        const {
-          progress,
-          download_url,
-          text
-        } = progressResponse.data;
-        return text === "Finished" ? download_url : (await new Promise(resolve => setTimeout(resolve, 1000)), checkProgress());
-      } catch (error) {
-        throw new Error("Error in progress check: " + error.message);
-      }
-    };
-    return await checkProgress();
-  } catch (error) {
-    console.error("Error:", error);
-    return {
-      'error': error.message
-    };
-  }
-}
-
-module.exports = {
-  'ytmp4': ytmp4
-};
 
 function extractYouTubeId(url) {
   const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/|playlist\?list=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
@@ -80,14 +13,26 @@ function extractYouTubeId(url) {
 
 function convertYouTubeLink(link) {
   const id = extractYouTubeId(link);
-  if (id) {
-    return "https://www.youtube.com/watch?v=" + id;
+  return id ? "https://www.youtube.com/watch?v=" + id : link;
+}
+
+async function getAudioUrl(videoUrl) {
+  try {
+    const response = await axios.get(`https://api.ryzendesu.vip/api/downloader/ytmp3?url=${encodeURIComponent(videoUrl)}`);
+    console.log("Réponse de l'API :", response.data); // Journal de la réponse de l'API
+    if (response.data && response.data.status === 'success') {
+      return response.data.result.url;
+    } else {
+      throw new Error('Failed to obtain audio URL.');
+    }
+  } catch (error) {
+    console.error("Erreur lors de l'obtention de l'URL de l'audio :", error);
+    throw new Error("Failed to obtain audio URL.");
   }
-  return link;
 }
 
 cmd({
-  'pattern': "play4",
+  'pattern': "play",
   'desc': "To download songs.",
   'react': '☃️',
   'category': "download",
@@ -118,12 +63,20 @@ cmd({
 }) => {
   try {
     if (!q) {
-      return reply("Please give me a URL or title.");
+      console.log("Aucun URL ou titre fourni.");
+      return reply("Please provide a URL or title.");
     }
     q = convertYouTubeLink(q);
+    console.log("Recherche de la vidéo YouTube pour :", q);
     const searchResults = await yts(q);
     const video = searchResults.videos[0];
+    if (!video) {
+      console.log("Aucune vidéo trouvée.");
+      return reply("No video found for the provided URL or title.");
+    }
     const videoUrl = video.url;
+    console.log("Vidéo trouvée:", videoUrl);
+
     const message = await conn.sendMessage(from, {
       image: {
         url: video.thumbnail
@@ -143,6 +96,7 @@ cmd({
       const isReplyToBot = message.message.extendedTextMessage && message.message.extendedTextMessage.contextInfo.stanzaId === messageId;
 
       if (isReplyToBot) {
+        console.log("Réponse reçue : ", text);
         await conn.sendMessage(remoteJid, {
           react: {
             text: '⬇️',
@@ -151,73 +105,75 @@ cmd({
         });
 
         try {
-          console.log("URL de la vidéo :", videoUrl);
-          const audioInfo = await fetchJson('https://api.ryzendesu.vip/api/downloader/ytmp3?url=' + videoUrl);
-          console.log("Réponse de l'API :", audioInfo);
-          if (audioInfo.status !== 'success') {
-            console.error("Erreur lors de l'obtention de l'URL de l'audio :", audioInfo.message);
-            return reply("Failed to obtain a valid audio URL.");
-          }
-          const audioUrl = audioInfo.result.download_url;
-          console.log("URL de l'audio :", audioUrl);
+          console.log("Obtention de l'URL de l'audio pour :", videoUrl);
+          const audioInfo = await axios.get(`https://api.ryzendesu.vip/api/downloader/ytmp3?url=${encodeURIComponent(videoUrl)}`);
+          console.log("Réponse de l'API :", audioInfo.data); // Journal de la réponse de l'API
 
-          await conn.sendMessage(remoteJid, {
-            delete: message.key
-          });
-          await conn.sendMessage(remoteJid, {
-            react: {
-              text: '⬆️',
-              key: message.key
-            }
-          });
+          if (audioInfo.data && audioInfo.data.status === 'success') {
+            const audioUrl = audioInfo.data.result.url;
+            console.log("URL de l'audio obtenue :", audioUrl);
 
-          if (text === '1') {
-            console.log("Envoi de l'audio.");
             await conn.sendMessage(remoteJid, {
-              audio: {
-                url: audioUrl
-              },
-              mimetype: "audio/mpeg",
-              contextInfo: {
-                externalAdReply: {
-                  title: video.title,
-                  body: video.videoId,
-                  mediaType: 1,
-                  sourceUrl: video.url,
-                  thumbnailUrl: video.thumbnail,
-                  renderLargerThumbnail: true,
-                  showAdAttribution: true
+              delete: message.key
+            });
+            await conn.sendMessage(remoteJid, {
+              react: {
+                text: '⬆️',
+                key: message.key
+              }
+            });
+
+            if (text === '1') {
+              console.log("Envoi de l'audio.");
+              await conn.sendMessage(remoteJid, {
+                audio: {
+                  url: audioUrl
+                },
+                mimetype: "audio/mpeg",
+                contextInfo: {
+                  externalAdReply: {
+                    title: video.title,
+                    body: video.videoId,
+                    mediaType: 1,
+                    sourceUrl: video.url,
+                    thumbnailUrl: video.thumbnail,
+                    renderLargerThumbnail: true,
+                    showAdAttribution: true
+                  }
                 }
-              }
-            }, {
-              quoted: message
-            });
-            await conn.sendMessage(remoteJid, {
-              react: {
-                text: '✅',
-                key: message.key
-              }
-            });
-          } else if (text === '2') {
-            console.log("Envoi du fichier audio.");
-            await conn.sendMessage(remoteJid, {
-              document: {
-                url: audioUrl
-              },
-              mimetype: "audio/mpeg",
-              fileName: `${video.title}.mp3`,
-              caption: "\n> *© Generated for you by KERM-MD-V1 ❤️*\n"
-            }, {
-              quoted: message
-            });
-            await conn.sendMessage(remoteJid, {
-              react: {
-                text: '✅',
-                key: message.key
-              }
-            });
+              }, {
+                quoted: message
+              });
+              await conn.sendMessage(remoteJid, {
+                react: {
+                  text: '✅',
+                  key: message.key
+                }
+              });
+            } else if (text === '2') {
+              console.log("Envoi du fichier audio.");
+              await conn.sendMessage(remoteJid, {
+                document: {
+                  url: audioUrl
+                },
+                mimetype: "audio/mpeg",
+                fileName: `${video.title}.mp3`,
+                caption: "\n> *© Generated for you by KERM-MD-V1 ❤️*\n"
+              }, {
+                quoted: message
+              });
+              await conn.sendMessage(remoteJid, {
+                react: {
+                  text: '✅',
+                  key: message.key
+                }
+              });
+            } else {
+              console.log("Texte inattendu reçu :", text);
+            }
           } else {
-            console.log("Texte inattendu reçu :", text);
+            console.error("Erreur lors de l'obtention de l'URL de l'audio :", audioInfo.data.message);
+            return reply("Failed to obtain a valid audio URL.");
           }
         } catch (err) {
           console.error("Erreur lors de l'obtention de l'URL de l'audio :", err);
@@ -226,7 +182,7 @@ cmd({
       }
     });
   } catch (error) {
-    console.log(error);
+    console.error("Erreur dans la commande play :", error);
     reply('' + error);
   }
 });
