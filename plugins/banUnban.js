a12deb1370dcb350bc0abd2f5803f6d789273798
KermRayan <kermd237@gmail.com>
2025-03-09 09:36:46 +0100

    Update banUnban.js

--- plugins/banUnban.js
+++ plugins/banUnban.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Path to the JSON file
-const bannedNumbersFilePath = path.resolve(__dirname, '../bannedNumbers.json');
+const bannedNumbersFilePath = path.resolve(__dirname, '../lib/diary.json');
 
 // Helper functions to read and write to the JSON file
 const readBannedNumbers = () => {
